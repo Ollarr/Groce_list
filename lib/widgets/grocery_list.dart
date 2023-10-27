22ae4ab3c095b74ce3e600a7dac867c67fd719d3
Oluwasegun <oluyedis@gmail.com>
2023-10-27 09:52:12 +0100

    Added the functionality to add the grocery item to the list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,26 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:grocelist/data/dummy_items.dart';
+// import 'package:grocelist/data/dummy_items.dart';
+import 'package:grocelist/models/grocery_item.dart';
+import 'package:grocelist/widgets/add_new_item.dart';
 
-class GroceryList extends StatelessWidget {
-  const GroceryList({super.key});
+class GroceryList extends StatefulWidget {
+  const GroceryList({
+    super.key,
+  });
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Grocery list"),
-      ),
-      body: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (ctx, index) => ListTile(
-                title: Text(groceryItems[index].name),
-                leading: Container(
-                  width: 24,
-                  height: 24,
-                  color: groceryItems[index].category.color,
-                ),
-                trailing: Text(groceryItems[index].quantity.toString()),
-              )),
+    final List<GroceryItem> newGroceryItems = [];
+    Widget content = const Center(
+      child: Text("No items aded yet"),
     );
+
+    if (newGroceryItems.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: newGroceryItems.length,
+          itemBuilder: (ctx, index) => Dismissible(
+                key: ValueKey(newGroceryItems[index].id),
+                onDismissed: (direction) {
+                  setState(() {
+                    newGroceryItems.remove(newGroceryItems[index]);
+                  });
+                },
+                child: ListTile(
+                  title: Text(newGroceryItems[index].name),
+                  leading: Container(
+                    width: 24,
+                    height: 24,
+                    color: newGroceryItems[index].category.color,
+                  ),
+                  trailing: Text(newGroceryItems[index].quantity.toString()),
+                ),
+              ));
+    }
+    void addItem() async {
+      final newItem = await Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const NewItem(),
+        ),
+      );
+      if (newItem == null) {
+        return;
+      }
+      setState(() {
+        newGroceryItems.add(newItem);
+      });
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Grocery list"),
+          actions: [
+            IconButton(onPressed: addItem, icon: const Icon(Icons.add))
+          ],
+        ),
+        body: content);
   }
 }
