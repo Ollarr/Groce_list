fd37d8a7d6f7738eee884b4787e8d2e73306a8b3
Oluwasegun <oluyedis@gmail.com>
2023-10-30 11:12:37 +0100

    Added the functionality  to add new grocery item to the backend

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: unused_element
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 // import 'package:grocelist/data/dummy_items.dart';
 import 'package:grocelist/models/grocery_item.dart';
 import 'package:grocelist/widgets/add_new_item.dart';
+
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({
@@ -16,43 +22,70 @@ class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     final List<GroceryItem> newGroceryItems = [];
+
+    void fetchGroceryItems() async {
+      final url = Uri.https(
+          "grocelist-31cb2-default-rtdb.firebaseio.com", "shopping-list.json");
+
+      final response = await http.get(url);
+      final groceryListData = json.decode(response.body);
+      for (final item in groceryListData.entries) {}
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      fetchGroceryItems();
+    }
+
+    void addItem() async {
+      // final newItem = await Navigator.of(context).push(
+      //   MaterialPageRoute(
+      //     builder: (ctx) => const NewItem(),
+      //   ),
+      // );
+      await Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const NewItem(),
+        ),
+      );
+
+      fetchGroceryItems();
+
+      // if (newItem == null) {
+      //   return;
+      // }
+      // setState(() {
+      //   newGroceryItems.add(newItem);
+      // });
+    }
+
     Widget content = const Center(
       child: Text("No items aded yet"),
     );
 
     if (newGroceryItems.isNotEmpty) {
-      content = ListView.builder(
-          itemCount: newGroceryItems.length,
-          itemBuilder: (ctx, index) => Dismissible(
-                key: ValueKey(newGroceryItems[index].id),
-                onDismissed: (direction) {
-                  setState(() {
-                    newGroceryItems.remove(newGroceryItems[index]);
-                  });
-                },
-                child: ListTile(
-                  title: Text(newGroceryItems[index].name),
-                  leading: Container(
-                    width: 24,
-                    height: 24,
-                    color: newGroceryItems[index].category.color,
+      content = Expanded(
+        child: ListView.builder(
+            itemCount: newGroceryItems.length,
+            itemBuilder: (ctx, index) => Dismissible(
+                  key: ValueKey(newGroceryItems[index].id),
+                  onDismissed: (direction) {
+                    setState(() {
+                      newGroceryItems.remove(newGroceryItems[index]);
+                    });
+                  },
+                  child: ListTile(
+                    title: Text(newGroceryItems[index].name),
+                    leading: Container(
+                      width: 24,
+                      height: 24,
+                      color: newGroceryItems[index].category.color,
+                    ),
+                    trailing: Text(newGroceryItems[index].quantity.toString()),
                   ),
-                  trailing: Text(newGroceryItems[index].quantity.toString()),
-                ),
-              ));
-    }
-    void addItem() async {
-      final newItem = await Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (ctx) => const NewItem(),
-        ),
+                )),
       );
-      if (newItem == null) {
-        return;
-      }
-      setState(() {
-        newGroceryItems.add(newItem);
-      });
     }
 
     return Scaffold(
